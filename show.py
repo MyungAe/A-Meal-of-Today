e814d5d326fe9fd9fbeec861514fc5848fbdbc25
YINGMEI <j.yeongmee@gmail.com>
2023-02-08 02:03:09 +0900

    삭제하기 기능 초기 구현

--- show.py
+++ show.py
@@ -17,12 +17,29 @@ app = Flask(__name__)
 def home():
     return render_template('show-resturants/index.html')
 
+
 @app.route("/show", methods=["POST"])
 def show():
     user_receive = request.form['user_give']
     restaurants = db.users.find_one({'user.nickname': user_receive}, {'_id': False})['user']['restaurants']
 
     return jsonify({'restaurants': restaurants})
+
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    user_receive = request.form['user_give']
+    index_receive = request.form['index_give']
+
+    restaurants = db.users.find_one({'user.nickname': user_receive}, {'_id': False})['user']['restaurants']
+
+    for i in range(len(restaurants)):
+        if int(restaurants[i]['index']) == int(index_receive):
+            del restaurants[i]
+
+    db.users.update_one({'user.nickname': user_receive}, {'$set': {'user.restaurants': restaurants}})
+
+    return jsonify({'msg': '테스트'})
 
 
 if __name__ == '__main__':
