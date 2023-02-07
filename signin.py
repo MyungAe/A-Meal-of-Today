72b1a934b15eac65c684a0227f62a21e7c981c70
MyungAe <ladiescheong@naver.com>
2023-02-07 17:30:01 +0900

    feat(signin.py, signup/index.html): 닉네임을 받아 디비에 저장하는 기능 구현

--- signin.py
+++ signin.py
@@ -1,51 +1,42 @@
 from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
-
 from pymongo import MongoClient
 import certifi
 
+app = Flask(__name__)
 ca = certifi.where()
-
 client = MongoClient('mongodb+srv://test:sparta@cluster0.g8d0ssb.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
 
-# ID, PW, NICK -> DB users에 자징
-
-# 메인 페이지
-@app.route('/')
+# GET : 홈페이지
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('signin/index.html')
+    return render_template('index.html')
 
-# 회원가입 페이지
-@app.route('/makeId')
-def singup():
+
+# GET : 회원가입
+@app.route('/signin')
+def signin():
     return render_template('signup/index.html')
 
-# 회원가입 정보 DB 저장
-@app.route('/makeNew', methods=["POST"])
-def makeNew():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
+
+# POST : 회원가입 정보 전달
+@app.route('/signin', methods=["POST"])
+def save_userdata():
     name_receive = request.form['name_give']
-
+    # print(name_receive)
     doc = {
-        'id': id_receive,
-        'password': pw_receive,
-        'nickname': name_receive
+        'user': {
+            'nickname': name_receive,
+            'restaurants': []
+        }
     }
-
     db.users.insert_one(doc)
     return jsonify({'msg': '회원가입 완료!'})
 
 
-
-
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-
-
-
+    app.run('0.0.0.0', port=5001, debug=True)
 
 # # 아이디 중복확인 GET
 # @app.route('/overlapId', methods=['GET'])
@@ -57,8 +48,6 @@ if __name__ == '__main__':
 # def overlapName():
 #     users_list = list(db.users.find({}, {'_id': False}))
 #     return jsonify({'users': users_list})
-
-
 
 # # 로그인 GET
 # @app.route('/signin', methods=["GET"])
