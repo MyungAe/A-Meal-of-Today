3a42a700a4ce703a1d9e164f5cd80bd729dd45c8
YINGMEI <j.yeongmee@gmail.com>
2023-02-08 04:20:06 +0900

    맛집 삭제하기 기능 ajax delete method로 수정 중
    초기 코드 (아직 기능 안 됨)

--- show.py
+++ show.py
@@ -26,19 +26,28 @@ def show():
     return jsonify({'restaurants': restaurants})
 
 
-@app.route("/delete", methods=["POST"])
+@app.route("/delete", methods=["DELETE"])
 def delete():
     user_receive = request.form['user_give']
     index_receive = request.form['index_give']
 
-    restaurants = db.users.find_one({'user.nickname': user_receive}, {'_id': False})['user']['restaurants']
+    print(user_receive, index_receive)
 
-    for i in range(len(restaurants)):
-        if int(restaurants[i]['index']) == int(index_receive):
-            del restaurants[i]
+    # (기능 함) =========== list를 가져와서 삭제할 부분 삭제하고 다시 그 list를 원래 자리에 update 하는 방식============
+    # restaurants = db.users.find({'user.nickname': user_receive}, {'_id': False})['user']['restaurants']
+    #
+    # for i in range(len(restaurants)):
+    #     if restaurants[i]['index'] == int(index_receive):
+    #         del restaurants[i]
+    #
+    # db.users.update_one({'user.nickname': user_receive}, {'$set': {'user.restaurants': restaurants}})
 
-    db.users.update_one({'user.nickname': user_receive}, {'$set': {'user.restaurants': restaurants}})
+    #(기능 안 됨) Mongodb의 $pull 쿼리를 써서 직접적으로 선택하여 업데이트(삭제) 하는 방법.
+    # db.users['user'].update_one({'nickname': user_receive}, {'$pull': {"restaurants": {'index': index_receive}}})
 
+    #(둘다 안 됨)
+    # db.users.delete_one({'user.restaurants': {'index': index_receive}})
+    # db.users['user']['restaurants'].delete_one({'index': index_receive})
     return jsonify({'msg': '테스트'})
 
 
